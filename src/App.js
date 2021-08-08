52872396ef2d6b6f12657388c33c3af4b0c46345
Sachin <apple@Apples-MacBook-Air.local>
2021-08-07 19:52:10 -0700

    Credit Card

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Card from './components/Card/Card';
+// import './App.css';
+import 'antd/dist/antd.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import chipImg from '../../images/chip.png';
+import visaImg from '../../images/visa.png';
+import masterCardImg from '../../images/mastercard.png';
+import amexImg from '../../images/amex.png';
+
+import './cards.css';
+
+export class Cards extends Component {
+    render() {
+        const {cardNumber, cardName, cardDate, cardCvv, cardNumFocused, cardNameFocused, cardDateFocused, cardCvvFocused, cardType } = this.props;
+        let titleImage = visaImg;
+        switch(cardType) {
+            case "mastercard" :
+                titleImage = masterCardImg
+                break;
+            case "amex" :
+                titleImage = amexImg
+                break;
+        }
+
+        return (
+            <div>
+                {
+                    !cardCvvFocused ? (
+                        <div className="credit-card-container">
+                            <div className="info-card-chip">
+                                <img src={chipImg} alt="" />
+                            </div>
+                            <div className="card-image">
+                                <img src={titleImage} />
+                            </div>
+                            <div className={`card-number ${cardNumFocused ? "bordered" : ''}`}>{cardNumber}</div>
+                            <div className={`card-name ${cardNameFocused ? "bordered" : ''} `}>
+                                <p className="info-title">Card Holder</p>
+                                <p className="card-holder">{cardName}</p>
+                            </div>
+                            <div className={`date-container ${cardDateFocused ? "bordered" : ''} `}>
+                                <p className="info-title">Expires</p>
+                                <p className="card-holder">{cardDate[0]+"/"+cardDate[1]}</p>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="credit-card-container">
+                            <div className="black-strip"></div>
+                            <p className="cvv-text">CVV</p>
+                            <div className="display-cvv">{cardCvv}</div>
+                            <div className="card-image-back">
+                                <img src={titleImage} />
+                            </div>
+                        </div> 
+                    )       
+                }
+            </div>
+            
+        )
+    }
+}
+
+export default Cards
